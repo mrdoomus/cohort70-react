5914779f929f8956d0f03c7a5a6f50982b19449b
Camilo Villegas <camilovillegas265@gmail.com>
2024-08-28 19:32:35 -0500

    august 28: adding fetch example

---
+++ src/fetch/FetchData.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+const FetchData = () => {
+  const [posts, setPosts] = useState([]);
+
+  // Logic section
+  async function fetchForPosts() {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+
+      const translatedPosts = await response.json();
+      setPosts(translatedPosts);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(fetchForPosts, []);
+
+  // UI section
+  return (
+    <div>
+      <h1>Requesting for Posts</h1>
+      {posts.length
+        ? posts.map((post) => <div>{post.title}</div>)
+        : "Loading..."}
+    </div>
+  );
+};
+
+export default FetchData;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { UseEffectExample } from "./useEffect/useEffectExample";
+import FetchData from "./fetch/FetchData";
 
 // After selecting the element with id = 'root'
 // we pass down this element to React so it knows where to start
 const htmlRoot = document.getElementById("root");
 const root = ReactDOM.createRoot(htmlRoot);
 
-root.render(<UseEffectExample />);
+root.render(<FetchData />);
